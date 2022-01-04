5d3e8bd6d5d6084c8e3f2c916114a2d1a5651e43
Cnamm7 <Cna.mm7@gmail.com>
2022-01-04 12:20:35 -0500

    completed dto managing objects

---
+++ Cardealership/src/main/java/com/sg/cardealership/dto/UserDto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.cardealership.dto;
+
+/**
+ *
+ * @author Cna
+ */
+public class UserDto {
+    private int userId;
+    private String userName;
+    private String password;
+    private AccountTypeDto account;
+}
---
+++ Cardealership/src/main/java/com/sg/cardealership/dto/VehicleDto.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.cardealership.dto;
+
+/**
+ *
+ * @author Cna
+ */
+public class VehicleDto {
+    private String vin;
+    private int year;
+    private String exteriorColor;
+    private String interiorColor;
+    private String bodyType;
+    private String transmission;
+    private String msrp;
+    private String salePrice;
+    
+    private ManufacturerDto manufacture;
+    private String photo;
+    private int mileage;
+    private String description;
+    
+}
