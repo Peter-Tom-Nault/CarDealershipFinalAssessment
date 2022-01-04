468eb6a16f948c0e14c5ae4de8b811107fa852d0
Cnamm7 <Cna.mm7@gmail.com>
2022-01-04 15:53:23 -0500

    completed a part of controller description

--- Cardealership/src/main/java/com/sg/cardealership/controller/carDealershipController.java
+++ Cardealership/src/main/java/com/sg/cardealership/controller/carDealershipController.java
@@ -1,5 +1,22 @@
 package com.sg.cardealership.controller;
 
 public class carDealershipController {
-
+    //home/index GET
+    //we need to return two jason feature and special
+    
+    
+    //inventory/new GET
+    //we should return jason of different objects, and decide what result we should return
+    
+    
+    //Home/Specials GET
+    //list of specials
+    
+    
+    //Home/Contact POST
+    //create a new data in contact info
+    
+    
+    //Sales/Index
+    //search function that only available to sales person
 }
