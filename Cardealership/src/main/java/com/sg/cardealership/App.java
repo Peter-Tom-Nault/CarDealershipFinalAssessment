201eb4ccef6f7e89148d97648dd492e90e3c323b
Cnamm7 <Cna.mm7@gmail.com>
2022-01-04 11:32:57 -0500

    created structure

--- Cardealership/src/main/java/com/sg/cardealership/App.java
+++ Cardealership/src/main/java/com/sg/cardealership/App.java
@@ -4,10 +4,16 @@
  */
 package com.sg.cardealership;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  *
  * @author Cna
  */
+@SpringBootApplication
 public class App {
-    
+        public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
