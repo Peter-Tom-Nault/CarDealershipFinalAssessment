e8addf66799a6a123af8547c8f9da3aa3eb2b0cb
David Chu <davidchu@outlook.com>
2022-01-07 10:17:05 -0500

    updated, Tanuj's contact page

--- Cardealership-User-Interface/assets/js/contact.js
+++ Cardealership-User-Interface/assets/js/contact.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
- 
+ alert("Hello! I am an alert box!!");
   addContact();
  
 });
@@ -52,27 +52,19 @@ function loadContacts() {
 // create function to collect data from #addForm and post it to the API service
 function addContact() {
   $("#submitbutton").click(function (event) {
-    // check for validation errors; abort function if there are errors
-    // var haveValidationErrors = checkAndDisplayValidationErrors(
-    //   $("#addForm").find("input")
-    // );
-
-    // if (haveValidationErrors) {
-    //   return false;
-    // }
-
+    alert("button clicked");
     // Ajax call to POST data to API service
     $.ajax({
       type: "POST",
-      url: "http://localhost:8080/home/contact",
+      url: "http://localhost:8080/home/Contact",
 
       // format data in JSON
       data: JSON.stringify({
-        firstName: "as",
-        lastName: "demo",
-        message: "demo",
-        email: "demo",
-        phone: 151550,
+        firstName: '$("#addFirstName").val()',
+        lastName:,' $("#addLastName").val()'
+        email: '$("#addCompany").v'al(')',
+        message: '$("#addEmail").val()',
+        phone: '$("#addPhone").val()',
       }),
       headers: {
         Accept: "application/json",
@@ -99,142 +91,4 @@ function addContact() {
   });
 }
 
-// clear Contact table prior to loading/updating data
-function clearContactTable() {
-  $("#contentRows").empty();
-}
 
-// open Edit Contact form and load data for selected record
-function showEditForm(contactId) {
-  $("#errorMessages").empty();
-
-  $.ajax({
-    type: "GET",
-    url:
-      "http://contactlist.us-east-1.elasticbeanstalk.com/contact/" + contactId,
-    success: function (data, status) {
-      $("#editFirstName").val(data.firstName);
-      $("#editLastName").val(data.lastName);
-      $("#editCompany").val(data.company);
-      $("#editPhone").val(data.phone);
-      $("#editEmail").val(data.email);
-      $("#editContactId").val(data.contactId);
-    },
-    error: function () {
-      $("#errorMessages").append(
-        $("<li>")
-          .attr({ class: "list-group-item list-group-item-danger" })
-          .text("Error calling web service. Please try again later.")
-      );
-    },
-  });
-
-  // hide the table when the form is opened
-  $("#contactTableDiv").hide();
-  $("#editFormDiv").show();
-}
-
-// when closing the Edit form, empty all fields, hide the form, and show the table
-function hideEditForm() {
-  $("#errorMessages").empty();
-
-  $("#editFirstName").val("");
-  $("#editLastName").val("");
-  $("#editCompany").val("");
-  $("#editPhone").val("");
-  $("#editEmail").val("");
-
-  $("#contactTableDiv").show();
-  $("#editFormDiv").hide();
-}
-
-// use a PUT request to update existing data
-function updateContact(contactId) {
-  $("#updateButton").click(function (event) {
-    // check for errors and abort if errors are found
-    var haveValidationErrors = checkAndDisplayValidationErrors(
-      $("#editForm").find("input")
-    );
-
-    if (haveValidationErrors) {
-      return false;
-    }
-
-    $.ajax({
-      type: "PUT",
-      url:
-        "http://contactlist.us-east-1.elasticbeanstalk.com/contact/" +
-        $("#editContactId").val(),
-
-      // format the data in JSON
-      data: JSON.stringify({
-        contactId: $("#editContactId").val(),
-        firstName: $("#editFirstName").val(),
-        lastName: $("#editLastName").val(),
-        company: $("#editCompany").val(),
-        phone: $("#editPhone").val(),
-        email: $("#editEmail").val(),
-      }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      dataType: "json",
-      success: function () {
-        $("#errorMessage").empty();
-        hideEditForm();
-        loadContacts();
-      },
-      error: function () {
-        $("#errorMessages").append(
-          $("<li>")
-            .attr({ class: "list-group-item list-group-item-danger" })
-            .text("Error calling web service. Please try again later.")
-        );
-      },
-    });
-  });
-}
-
-// delete record with corresponding contactId value
-function deleteContact(contactId) {
-  $.ajax({
-    type: "DELETE",
-    url:
-      "http://contactlist.us-east-1.elasticbeanstalk.com/contact/" + contactId,
-    success: function () {
-      loadContacts();
-    },
-  });
-}
-
-// collect and display data validation error messages
-function checkAndDisplayValidationErrors(input) {
-  $("#errorMessages").empty();
-
-  var errorMessages = [];
-
-  // generate specific message using form label and browser-generated errors messages
-  input.each(function () {
-    if (!this.validity.valid) {
-      var errorField = $("label[for=" + this.id + "]").text();
-      errorMessages.push(errorField + " " + this.validationMessage);
-    }
-  });
-
-  // display messages in #errorMessages div
-  if (errorMessages.length > 0) {
-    $.each(errorMessages, function (index, message) {
-      $("#errorMessages").append(
-        $("<li>")
-          .attr({ class: "list-group-item list-group-item-danger" })
-          .text(message)
-      );
-    });
-    // return true, indicating that there were errors
-    return true;
-  } else {
-    // return false, indicating that there were no errors
-    return false;
-  }
-}
