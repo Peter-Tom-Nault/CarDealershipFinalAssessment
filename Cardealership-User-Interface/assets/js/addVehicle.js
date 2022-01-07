8483224134a295fa1b121e79737d5eac972bee60
Peter-Tom-Nault <pt.nault@gmail.com>
2022-01-07 03:39:32 -0600

    Closer to working but currently causes io exception so don't use the add page without changes

--- Cardealership-User-Interface/assets/js/addVehicle.js
+++ Cardealership-User-Interface/assets/js/addVehicle.js
@@ -26,5 +26,48 @@ function populateModels(){
 }
 
 function populateOptionals(){
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/home/admin/option',
+        success: function(optionalList){
+            $.each(optionalList, function(index, optionalFeat){
+                $('#optionals').append('<option value="' + optionalFeat.featureId+'">'+optionalFeat.name+'</option>')
+            })
+        }
+    })
     
 }
+
+$('#addButton').on('click', function() {
+    newModel = $('#model').val();
+    optFeat = $('#optionals').val();
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/home/Admin/AddVehicle',
+        data: JSON.stringify({
+            vin: $('#vin').val(),
+            year: $('#year').val(),
+            exteriorColor: $('#color'),
+            interiorColor: $('#interior'),
+            bodyType: $('#bodystyle'),
+            transmission: $('#transmission'),
+            msrp: $('#msrp'),
+            salePrice: $('#salesprice'),
+            photo: newModel.id+".jpg",
+            mileage: $('#mileage'),
+            description: $('#message'),
+            purchased: false,
+            featured: false,
+            model: newModel,
+            optional: optFeat
+        }),
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+       },
+       success: function(){
+
+       }
+    })
+})
--- Cardealership/src/main/java/com/sg/cardealership/config/WebSecurityConfig.java
+++ Cardealership/src/main/java/com/sg/cardealership/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-    	http.cors();
+    	http.cors().and().csrf().disable();;
     }
 
     @Bean
